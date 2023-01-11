33ffb67d916a4dc3be3da7ed3e33e8d1eea313fb
George <geo264@mail.ru>
2023-01-11 21:03:32 +0300

    add form

--- src/components/Authorization/Authorization.tsx
+++ src/components/Authorization/Authorization.tsx
@@ -1,11 +1,56 @@
 	import styles from './Authorization.module.scss';
 
+	import { useForm, SubmitHandler } from "react-hook-form";
+
+	import {ReactComponent as IconLogin} from '../../assets/img/authorization/iconLogin.svg';
+	import {ReactComponent as IconPassword} from '../../assets/img/authorization/iconPassword.svg';
+	 
+	interface IFormInputs {
+		firstName: string;
+		lastName: string;
+	 }
+	 const onSubmit: SubmitHandler<IFormInputs> = data => console.log(data);
+
 	const Authorization = () => {
+		const { register, formState: { errors }, handleSubmit } = useForm<IFormInputs>();
+
+		/* const error = errors.firstName ? {"outline": "2px solid rgba(78, 100, 249, 0.8)"}: {"outline": "none"}; */
+
 		return (
 			<>
 				<div className={styles.wrapper}>
 					<div className={styles.content}>
 						<h1 className={styles.title}>Авторизация</h1>
+						<div className={styles.descr}>
+							Авторизируйтесь, чтобы начать
+							<br />
+							публиковать свои объявления
+						</div>
+						<form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+							<input 
+								className={styles.login} 
+								type='text' 
+								{...register("firstName", { required: true })} 
+								placeholder="Логин"
+							/>
+							 <IconLogin className={styles.iconLogin}/>
+							<input 
+								className={styles.password} 
+								type='password'  
+								{...register("lastName", { required: true })}
+								placeholder="Пароль"
+							/>
+							 {errors.firstName && "Password is required"}
+							 <IconPassword className={styles.iconPassword}/>
+							<div className={styles.bloks}>
+								<div className={styles.checkBoxText}>Запомнить меня</div>
+								<div className={styles.restorePassword}>Забыли пароль?</div>
+							</div>
+							<button className={styles.FormBtn} type='submit'>Войти</button>
+						</form>
+						<div className={styles.checkIn}>
+							Еще нет аккаунта? <span className={styles.link}>Создайте акканут</span>
+						</div>
 					</div>
 				</div>
 			</>
